14c377d4b69ab4c0643ed7772d7d71a91193c934
Alexandre Nicolas <nicolas.alexandre@creatic-agency.fr>
2019-12-02 12:51:07 +0100

    add Provider

---
+++ src/HandlerGeneratorProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace nicolasalexandre9\HandlerGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class HandlerGeneratorProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
