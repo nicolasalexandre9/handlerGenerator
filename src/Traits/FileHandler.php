58752627801e7bda3e0eab6d1a6ea48e204d22f1
Alexandre Nicolas <nicolas.alexandre@creatic-agency.fr>
2020-06-12 17:13:04 +0200

    update generate pattern.php

--- src/Traits/FileHandler.php
+++ src/Traits/FileHandler.php
@@ -21,11 +21,30 @@ trait FileHandler
      */
     public function updatePatternConfig(string $name): bool
     {
+        $replaces = [
+            '\\\\'    => '\\',
+            'array (' => '[',
+            ')'       => ']',
+            '\''      => '',
+        ];
         $pattern = config('pattern');
         $filename = config_path(). '/pattern.php';
         if (isset($pattern) && file_exists($filename)) {
-            $pattern['App\\Http\\Handlers\\Interfaces\\'.ucfirst($name).'HandlerInterface'] = 'App\\Http\\Handlers\\Core\\'.ucfirst($name).'Handler';
-            return (bool)file_put_contents($filename, '<?php return ' . var_export($pattern, true) . ';');
+            $pattern["App\\Http\\Handlers\\Interfaces\\".ucfirst($name).'HandlerInterface'] = 'App\\Http\\Handlers\\Core\\'.ucfirst($name).'Handler';
+            //Add ::class for all className
+            foreach ($pattern as $key => $value) {
+                $pattern[$key . '::class'] = $value . '::class';
+                unset($pattern[$key]);
+            }
+
+            // create the string to put in file
+            $pattern = var_export($pattern, true);
+            foreach ($replaces as $search => $replace) {
+                $pattern = str_replace($search, $replace, $pattern);
+            }
+            $pattern = "<?php \n\rreturn " . $pattern . ";";
+
+            return (bool)file_put_contents($filename, $pattern);
         } else {
             return false;
         }
